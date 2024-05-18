41ef980a5652f9a62e5c3b57d934a26945b2fdbe
Gipfeli <luis.haenny@proton.me>
2024-05-18 10:13:34 +0200

    added some stuff

--- src/app/tab-group/tab.group.component.ts
+++ src/app/tab-group/tab.group.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChildren, AfterContentInit, QueryList, ElementRef } from '@angular/core';
+import {Component, ContentChildren, AfterContentInit, QueryList, ElementRef, ViewChild} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -8,10 +8,8 @@ import { TabComponent } from '../tab/tab.component';
 })
 export class TabGroupComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
-  @ContentChildren('tabDiv', {read: ElementRef}) tabDivs!: QueryList<ElementRef>
-
+  @ViewChild('tabDiv') tabDivs!: QueryList<ElementRef>;
   selected = 0;
-  barHover = false;
 
   ngAfterContentInit() {
     const activeTabs = this.tabs.filter(tab => tab.active);
@@ -21,17 +19,6 @@ export class TabGroupComponent implements AfterContentInit {
   }
 
   selectTab(index: number) {
-    const divArray = this.tabDivs.toArray()
-    const width = divArray[index].nativeElement.offsetWidth;
     this.selected = index;
-    this.barHover = true;
-
-    console.log(width)
-  }
-
-  mouseEnter(index: number){
-    if (index === this.selected){
-      this.barHover = true;
-    }
   }
 }
